e0511ba60022ce139d1fd2ddc04f42cb6b2871dc
Mateo pizzolo <mateo@Mateos-Mac-mini.local>
2023-10-12 16:11:17 +0200

    Adding custom hooks for form input fields

--- frontend/src/features/auth/components/RegistrationFrom.component.tsx
+++ frontend/src/features/auth/components/RegistrationFrom.component.tsx
@@ -1,13 +1,73 @@
 import { Box, Grid, TextField, InputLabel, Typography, Button, Divider } from '@mui/material'
 import { FC, FormEvent } from 'react'
 import { Link } from 'react-router-dom';
+import useInput from '../../../hooks/input/use-input';
+import { validateNameLength, validatePasswordLength } from '../../../shared/utils/validation/length';
+import { validateEmail } from '../../../shared/utils/validation/email';
+import { NewUser } from './models/NewUser';
 
 const RegistrationFromComponent: FC = () => {
+	const {
+		text: name,
+		shouldDisplayError: nameHasError,
+		textChangeHandler: nameChangeHandler,
+		inputBlurHandler: nameBlurHandler,
+		clearHandler: nameClearHandler,
+	} = useInput(validateNameLength);
+
+	const {
+		text: email,
+		shouldDisplayError: emailHasError,
+		textChangeHandler: emailChangeHandler,
+		inputBlurHandler: emailBlurHandler,
+		clearHandler: emailClearHandler,
+	} = useInput(validateEmail);
+
+	const {
+		text: password,
+		shouldDisplayError: passwordHasError,
+		textChangeHandler: passwordChangeHandler,
+		inputBlurHandler: passwordBlurHandler,
+		clearHandler: passwordClearHandler,
+	} = useInput(validatePasswordLength);
+
+	const {
+		text: confirmPassword,
+		shouldDisplayError: confirmPasswordHasError,
+		textChangeHandler: confirmPasswordChangeHandler,
+		inputBlurHandler: confirmPasswordBlurHandler,
+		clearHandler: confirmPasswordClearHandler,
+	} = useInput(validatePasswordLength);
+
+	const clearForm = () => {
+		nameClearHandler();
+		emailClearHandler();
+		passwordClearHandler();
+		confirmPasswordClearHandler();
+	};
 
 	const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log("clicked");
-	}
+
+		if (password !== confirmPassword) return;
+
+		if (nameHasError || emailHasError || passwordHasError || confirmPasswordHasError)
+			return;
+
+		if (name.length === 0 || email.length === 0 || password.length === 0 || confirmPassword.length === 0)
+			return;
+
+		const newUser: NewUser = {
+			name,
+			email,
+			password,
+		};
+
+		// dispatch(register(newUser));
+	};
+
+	// if (isLoading)
+	// 	return <CircularProgress sx={{ marginTop: '64px' }} color='primary' />;
 
 	return (
 		<Box sx={{ border: 1, padding: 2, borderColor: '#cccccc', width: '350px' }}>
@@ -16,21 +76,21 @@ const RegistrationFromComponent: FC = () => {
 					<Typography variant="h4" component="h1">Create Account</Typography>
 
 					<InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="name"> Your name</InputLabel>
-					<TextField type="text" name="name" id="name" variant="outlined" size="small" />
+					<TextField value={name} onChange={nameChangeHandler} onBlur={nameBlurHandler} error={nameHasError} helperText={nameHasError ? "Enter your name" : ""} type="text" name="name" id="name" variant="outlined" size="small" />
 
 					<InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="email"> Your email</InputLabel>
-					<TextField type="text" name="email" id="email" variant="outlined" size="small" />
+					<TextField value={email} onChange={emailChangeHandler} onBlur={emailBlurHandler} error={emailHasError} helperText={emailHasError ? "Enter your email" : ""} type="email" name="email" id="email" variant="outlined" size="small" />
 
 					<InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="password"> Your password</InputLabel>
-					<TextField type="text" name="password" id="password" variant="outlined" size="small" placeholder='Minimum 6 characters required ' />
+					<TextField value={password} onChange={passwordChangeHandler} onBlur={passwordBlurHandler} error={passwordHasError} helperText={passwordHasError ? "Min 6 characters" : ""} type="password" name="password" id="password" variant="outlined" size="small" placeholder='Minimum 6 characters required ' />
 
 					<InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="confirmPassword"> Re-Enter password</InputLabel>
-					<TextField type="text" name="name" id="confirmPassword" variant="outlined" size="small" />
+					<TextField value={confirmPassword} onChange={confirmPasswordChangeHandler} onBlur={confirmPasswordBlurHandler} error={confirmPassword.length > 0 && password !== confirmPassword} helperText={confirmPassword.length > 0 && password !== confirmPassword ? "Passwords must match" : ""} type="password" name="name" id="confirmPassword" variant="outlined" size="small" />
 
 					<Button variant="contained" type="submit" style={{ marginTop: "16px", height: "31px", backgroundColor: "#f0c14b", color: "black", borderColor: "#a88734 #9c7e31 #846a29", textTransform: "none" }}>Register</Button>
 				</Grid>
 			</form>
-			
+
 			<div style={{ marginTop: "30px" }}>
 				<small>
 					<span>By creating an account, you agree to Amazon´s</span>
@@ -46,7 +106,7 @@ const RegistrationFromComponent: FC = () => {
 			<div>
 				<small>
 					Already have an account?{' '}
-					<Link to="/signin" style={{textDecoration: "none", color: "#0000ee"}}>Sign-in</Link>
+					<Link to="/signin" style={{ textDecoration: "none", color: "#0000ee" }}>Sign-in</Link>
 				</small>
 			</div>
 
--- frontend/src/features/auth/components/SigninForm.component.tsx
+++ frontend/src/features/auth/components/SigninForm.component.tsx
@@ -1,69 +1,100 @@
 import { Box, Grid, TextField, InputLabel, Typography, Button, Divider } from '@mui/material'
 import { FC, FormEvent } from 'react'
 import { Link } from 'react-router-dom';
+import useInput from '../../../hooks/input/use-input';
+import { validateNameLength, validatePasswordLength } from '../../../shared/utils/validation/length';
+import { validateEmail } from '../../../shared/utils/validation/email';
+import { NewUser } from './models/NewUser';
 
 const SigninFormComponent: FC = () => {
+	const {
+		text: email,
+		shouldDisplayError: emailHasError,
+		textChangeHandler: emailChangeHandler,
+		inputBlurHandler: emailBlurHandler,
+		clearHandler: emailClearHandler,
+	} = useInput(validateEmail);
 
-    const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("clicked");
-    }
+	const {
+		text: password,
+		shouldDisplayError: passwordHasError,
+		textChangeHandler: passwordChangeHandler,
+		inputBlurHandler: passwordBlurHandler,
+		clearHandler: passwordClearHandler,
+	} = useInput(validatePasswordLength);
 
-    return (
-        <>
-            <Box sx={{ border: 1, padding: 2, borderColor: '#cccccc', width: '350px' }}>
-                <form onSubmit={onSubmitHandler}>
-                    <Grid container direction="column" justifyContent="flex-start">
-                        <Typography variant="h4" component="h1">Sign-In</Typography>
+	const clearForm = () => {
+		emailClearHandler();
+		passwordClearHandler();
+	};
 
-                        <InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="email"> Your email</InputLabel>
-                        <TextField type="text" name="email" id="email" variant="outlined" size="small" />
+	const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-                        <InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="password"> Your password</InputLabel>
-                        <TextField type="text" name="password" id="password" variant="outlined" size="small" placeholder='Minimum 6 characters required ' />
+		if (emailHasError || passwordHasError)
+			return;
 
-                        <Button variant="contained" type="submit" style={{ marginTop: "16px", height: "31px", backgroundColor: "#f0c14b", color: "black", borderColor: "#a88734 #9c7e31 #846a29", textTransform: "none" }}>Sign-In</Button>
-                    </Grid>
-                </form>
+		if (email.length === 0 || password.length === 0)
+			return;
 
-                <div style={{ marginTop: "30px" }}>
-                    <small>
-                        <span>By continuing, you agree to Amazon´s</span>
-                    </small>
-                </div>
-                <div>
-                    <small>
-                        <a href="#" style={{ textDecoration: "none" }}>{' '}Conditions of use</a>{' '}and{' '}<a href="#" style={{ textDecoration: "none" }}>Privacy Policy</a>
-                    </small>
-                </div>
-            </Box>
-            <div style={{ marginTop: '16px' }}>
-                <Divider>
-                    <small style={{ color: '#767676' }}>New to Amazon?</small>
-                </Divider>
+		clearForm();
+	};
 
-                <Link
-                    id='register-link'
-                    to='/register'
-                    style={{ textDecoration: 'none', color: '#0000ee' }}
-                >
-                    <Button
-                        variant='contained'
-                        style={{
-                            width: '100%',
-                            marginTop: '12px',
-                            height: '31px',
-                            backgroundColor: '#f1f1f1',
-                            color: 'black',
-                            textTransform: 'none',
-                        }}
-                    >
-                        Register
-                    </Button>
-                </Link>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<Box sx={{ border: 1, padding: 2, borderColor: '#cccccc', width: '350px' }}>
+				<form onSubmit={onSubmitHandler}>
+					<Grid container direction="column" justifyContent="flex-start">
+						<Typography variant="h4" component="h1">Sign-In</Typography>
+
+						<InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="email"> Your email</InputLabel>
+						<TextField value={email} onChange={emailChangeHandler} onBlur={emailBlurHandler} error={emailHasError} helperText={emailHasError ? "Enter your email" : ""} type="email" name="email" id="email" variant="outlined" size="small" />
+
+						<InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "black" }} htmlFor="password"> Your password</InputLabel>
+						<TextField value={password} onChange={passwordChangeHandler} onBlur={passwordBlurHandler} error={passwordHasError} helperText={passwordHasError ? "Min 6 characters" : ""} type="password" name="password" id="password" variant="outlined" size="small" placeholder='Minimum 6 characters required ' />
+
+						<Button variant="contained" type="submit" style={{ marginTop: "16px", height: "31px", backgroundColor: "#f0c14b", color: "black", borderColor: "#a88734 #9c7e31 #846a29", textTransform: "none" }}>Sign-In</Button>
+					</Grid>
+				</form>
+
+				<div style={{ marginTop: "30px" }}>
+					<small>
+						<span>By continuing, you agree to Amazon´s</span>
+					</small>
+				</div>
+				<div>
+					<small>
+						<a href="#" style={{ textDecoration: "none" }}>{' '}Conditions of use</a>{' '}and{' '}<a href="#" style={{ textDecoration: "none" }}>Privacy Policy</a>
+					</small>
+				</div>
+			</Box>
+			<div style={{ marginTop: '16px' }}>
+				<Divider>
+					<small style={{ color: '#767676' }}>New to Amazon?</small>
+				</Divider>
+
+				<Link
+					id='register-link'
+					to='/register'
+					style={{ textDecoration: 'none', color: '#0000ee' }}
+				>
+					<Button
+						variant='contained'
+						style={{
+							width: '100%',
+							marginTop: '12px',
+							height: '31px',
+							backgroundColor: '#f1f1f1',
+							color: 'black',
+							textTransform: 'none',
+						}}
+					>
+						Register
+					</Button>
+				</Link>
+			</div>
+		</>
+	)
 }
 
 export default SigninFormComponent
